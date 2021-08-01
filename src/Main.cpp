429e5c58f9be624c1e0ab35ffba6faddb90f2144
Daniil Demidko <demidko@farpost.com>
2021-08-02 04:28:45 +1000

    wip: http server

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
 
-int main(int argc, char **argv) {
-  std::cout << "It works" << std::endl;
+#define HTTPSERVER_IMPL
+
+#include "httpserver.h"
+
+int main() {
+  http_server_listen(http_server_init(80, [](http_request_s *const request) {
+    auto response = http_response_init();
+    std::string text = "asa";
+    http_response_status(response, 200);
+    http_response_header(response, "Content-Type", "text/plain");
+    http_response_body(response, text.c_str(), size(text));
+    auto x = http_request_body(request);
+
+    spdlog::info("{}", (char *) request->data);
+    http_respond(request, response);
+  }));
 }
